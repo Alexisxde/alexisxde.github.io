0cba8eb7cb6d5a2955a69c6fe7c754955721b127
Alexisxde <olivarezalexis749@gmail.com>
2024-04-14 17:30:34 -0300

    feat: landing page deployment with astro on github page ✨

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from '@astrojs/tailwind'
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://alexis.github.io',
-  base: 'my-repo'
+  base: 'https://github.com/Alexisxde/alexis.github.io'
 })
