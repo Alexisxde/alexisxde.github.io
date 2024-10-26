c5890be387ba455452f870f5dba9a26a4ea49454
Alexisxde <olivarezalexis749@gmail.com>
2024-10-25 23:06:13 -0300

    Varibles TailwindCSS

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,6 +6,14 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      colors: {
+        dark: '#0c0a09',
+        light: '#fbfbfb',
+        'border-light': '#e7e5e4',
+        'border-dark': '#292524',
+        'gray-light': '#78716c',
+        'gray-dark': '#a8a29e'
+      },
       fontFamily: {
         sans: ['Poppins', ...defaultTheme.fontFamily.sans]
       },
