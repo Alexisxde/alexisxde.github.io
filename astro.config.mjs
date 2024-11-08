a7038ebc01542a39421b98a455e8066677f3d652
Alexisxde <olivarezalexis749@gmail.com>
2024-11-08 14:56:45 -0300

    Skills Icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { rehypeHeadingIds } from '@astrojs/markdown-remark'
 import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), icon()],
   site: 'https://alexisxde.github.io',
   trailingSlash: 'never',
   build: {
